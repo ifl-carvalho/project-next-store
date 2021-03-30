49e03ef15c37e5c52ab20ca690a242f0ee34bc97
ifaithfrost <ifaithfrost@gmail.com>
2021-03-30 16:07:26 -0300

    diretory changes

--- tests/footer.unit.test.tsx
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer'
-import Footer from '../components/footer'
-
-describe('Footer Component', () => {
-  it('should match the snapshot', () => {
-    const tree = renderer.create(<Footer />).toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-})
